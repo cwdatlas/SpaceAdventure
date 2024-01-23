ea8ad7a5a4c971d80f08015f5d6f61ddbbf0b092
cwdatlas <ascott@carroll.edu>
2024-01-22 19:53:33 -0700

    updated base classes

--- Models/BaseEngine.py
+++ Models/BaseEngine.py
@@ -7,9 +7,12 @@ class Engine():
     thrust: int
     # in seconds
     isp: int
+    # in meters
+    width: float
 
-    def __init__(self, name, mass, thrust, isp):
+    def __init__(self, name, mass, thrust, isp, width):
         self.name = name
         self.mass = mass
         self.thrust = thrust
         self.isp = isp
+        self.width = width
--- Models/BaseTank.py
+++ Models/BaseTank.py
@@ -4,8 +4,11 @@ class Tank():
     mass: int
     # in kg (fuel mass)
     liquid_fuel: int
+    # in meters
+    width: float
 
-    def __init__(self, name, mass, liquid_fuel):
+    def __init__(self, name, mass, liquid_fuel, width):
         self.name = name
         self.mass = mass
         self.liquid_fuel =liquid_fuel
+        self.width = width
